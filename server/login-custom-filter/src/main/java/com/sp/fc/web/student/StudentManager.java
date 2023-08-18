13ebe3efe9e3020bdb8b0c83617a2de9c333997e
yurison <yu_ri_son@naver.com>
2023-08-18 22:19:18 +0900

    Token 커스터마이징

--- server/login-custom-filter/src/main/java/com/sp/fc/web/student/StudentManager.java
+++ server/login-custom-filter/src/main/java/com/sp/fc/web/student/StudentManager.java
@@ -18,10 +18,10 @@ public class StudentManager implements AuthenticationProvider, InitializingBean 
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
+        StudentAuthenticationToken token = (StudentAuthenticationToken) authentication;
 
-        if (studentDB.containsKey(token.getName())) {
-            Student student = studentDB.get(token.getName());
+        if (studentDB.containsKey(token.getCredentials())) {
+            Student student = studentDB.get(token.getCredentials());
             return StudentAuthenticationToken.builder()
                     .principal(student)
                     .details(student.getUsername())
@@ -34,7 +34,7 @@ public class StudentManager implements AuthenticationProvider, InitializingBean 
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return authentication == UsernamePasswordAuthenticationToken.class;
+        return authentication == StudentAuthenticationToken.class;
     }
 
     @Override
--- server/login-custom-filter/src/main/java/com/sp/fc/web/teacher/TeacherManager.java
+++ server/login-custom-filter/src/main/java/com/sp/fc/web/teacher/TeacherManager.java
@@ -18,10 +18,10 @@ public class TeacherManager implements AuthenticationProvider, InitializingBean 
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
+        TeacherAuthenticationToken token = (TeacherAuthenticationToken) authentication;
 
-        if (teacherDB.containsKey(token.getName())) {
-            Teacher teacher = teacherDB.get(token.getName());
+        if (teacherDB.containsKey(token.getCredentials())) {
+            Teacher teacher = teacherDB.get(token.getCredentials());
             return TeacherAuthenticationToken.builder()
                     .principal(teacher)
                     .details(teacher.getUsername())
@@ -34,7 +34,7 @@ public class TeacherManager implements AuthenticationProvider, InitializingBean 
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return authentication == UsernamePasswordAuthenticationToken.class;
+        return authentication == TeacherAuthenticationToken.class;
     }
 
     @Override
