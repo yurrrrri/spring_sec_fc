89666c3a27351076ffeac3d92ae34bd37d0bb543
yurison <yu_ri_son@naver.com>
2023-08-18 11:19:05 +0900

    CustomLoginFilter

--- server/login-custom-filter/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/login-custom-filter/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity(debug = true)
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -30,13 +31,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CustomLoginFilter filter = new CustomLoginFilter(authenticationManager());
         http
                 .authorizeRequests(request ->
-                        request.antMatchers("/").permitAll()
+                        request.antMatchers("/", "/login").permitAll()
                                 .anyRequest().authenticated())
-                .formLogin(login -> login.loginPage("/login").permitAll()
-                        .defaultSuccessUrl("/", false)
-                        .failureUrl("/login-error"))
+//                .formLogin(login -> login.loginPage("/login").permitAll()
+//                        .defaultSuccessUrl("/", false)
+//                        .failureUrl("/login-error"))
+                .addFilterAt(filter, UsernamePasswordAuthenticationFilter.class)
                 .logout(logout -> logout.logoutSuccessUrl("/"))
                 .exceptionHandling(e -> e.accessDeniedPage("/access-denied"));
     }
