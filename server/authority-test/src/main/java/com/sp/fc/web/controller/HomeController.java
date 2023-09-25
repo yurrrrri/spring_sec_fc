95439cfba4891b9c9fe8250f399576f5a9e3985a
yurison <yu_ri_son@naver.com>
2023-09-25 23:47:37 +0900

    서비스 단에서의 권한 체크

--- server/authority-test/src/main/java/com/sp/fc/web/controller/HomeController.java
+++ server/authority-test/src/main/java/com/sp/fc/web/controller/HomeController.java
@@ -1,16 +1,28 @@
 package com.sp.fc.web.controller;
 
+import com.sp.fc.web.service.SecurityMessageService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final SecurityMessageService securityMessageService;
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/greeting")
     public String greeting() {
         return "Hello";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/greeting/{name}")
+    public String greeting(@PathVariable String name) {
+        return "Hello " + securityMessageService.message(name);
     }
 
 }
--- server/authority-test/src/test/java/com/sp/fc/web/test/AuthorityBasicTest.java
+++ server/authority-test/src/test/java/com/sp/fc/web/test/AuthorityBasicTest.java
@@ -1,6 +1,5 @@
 package com.sp.fc.web.test;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +10,19 @@ class AuthorityBasicTest extends WebIntegrationTest {
 
     TestRestTemplate client;
 
-    @DisplayName("greeting 메시지를 불러온다.")
     @Test
-    void test_1() {
+    void greetingTest() {
         client = new TestRestTemplate("user1", "1111");
         ResponseEntity<String> response = client.getForEntity(uri("/greeting"), String.class);
 
         assertEquals("Hello", response.getBody());
+    }
+
+    @Test
+    void greetingWithNameTest() {
+        client = new TestRestTemplate("user1", "1111");
+        ResponseEntity<String> response = client.getForEntity(uri("/greeting/yuri"), String.class);
+
+        assertEquals("Hello yuri", response.getBody());
     }
 }
