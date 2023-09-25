c82a524465c4916b3f928fc59e94acbb5c713de8
yurison <yu_ri_son@naver.com>
2023-09-25 23:09:55 +0900

    AuthorityBasicTest1

--- server/authority-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/authority-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -1,4 +1,30 @@
 package com.sp.fc.web.config;
 
-public class SecurityConfig {
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser(User.withDefaultPasswordEncoder()
+                        .username("user1")
+                        .password("1111")
+                        .roles("USER"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .httpBasic().and()
+                .authorizeRequests(authority -> authority.anyRequest().authenticated());
+    }
+
 }
--- test/web-util/src/main/java/com/sp/fc/web/test/WebIntegrationTest.java
+++ test/web-util/src/main/java/com/sp/fc/web/test/WebIntegrationTest.java
@@ -4,7 +4,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 
 import static java.lang.String.format;
 
@@ -14,8 +13,13 @@ public class WebIntegrationTest {
     @LocalServerPort
     int port;
 
-    public URI uri(String path) throws URISyntaxException {
-        return new URI(format("http://localhost:%d%s", port, path));
+    public URI uri(String path) {
+        try {
+            return new URI(format("http://localhost:%d%s", port, path));
+        } catch (Exception ex) {
+            throw new IllegalArgumentException();
+        }
+
     }
 
 }
