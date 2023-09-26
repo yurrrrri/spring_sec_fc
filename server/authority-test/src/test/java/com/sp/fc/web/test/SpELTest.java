65689a75523628059916fc7004d5330efebb8148
yurison <yu_ri_son@naver.com>
2023-09-26 17:53:26 +0900

    SpELTest

--- server/authority-test/src/test/java/com/sp/fc/web/test/SpELTest.java
+++ server/authority-test/src/test/java/com/sp/fc/web/test/SpELTest.java
@@ -5,8 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.expression.AccessException;
+import org.springframework.expression.BeanResolver;
+import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,6 +18,7 @@ class SpELTest {
 
     ExpressionParser parser = new SpelExpressionParser();
     Person person = Person.builder().name("yuri").height(163).build();
+    Cat cat = Cat.builder().name("nancy").height(60).build();
 
     @Test
     void basicTest() {
@@ -30,7 +35,36 @@ class SpELTest {
     void overMethodTest() {
         assertEquals(
                 Boolean.TRUE,
-                parser.parseExpression("over(160)").getValue(person, Boolean.class)
+                parser.parseExpression("over(100)").getValue(person, Boolean.class)
+        );
+    }
+
+    @Test
+    void overMethodTest2() {
+        assertEquals(
+                Boolean.FALSE,
+                parser.parseExpression("over(100)").getValue(cat, Boolean.class)
+        );
+    }
+
+    @Test
+    void contextTest() {
+        StandardEvaluationContext ctx = new StandardEvaluationContext();
+        ctx.setBeanResolver((context, beanName) -> beanName.equals("person") ? person : cat);
+        ctx.setRootObject(person);
+        ctx.setVariable("may", cat);
+
+        assertEquals(
+                Boolean.TRUE,
+                parser.parseExpression("@person.over(100)").getValue(ctx, Boolean.class)
+        );
+        assertEquals(
+                Boolean.FALSE,
+                parser.parseExpression("@cat.over(100)").getValue(ctx, Boolean.class)
+        );
+        assertEquals(
+                Boolean.FALSE,
+                parser.parseExpression("#may.over(100)").getValue(ctx, Boolean.class)
         );
     }
 }
@@ -40,6 +74,19 @@ class SpELTest {
 @NoArgsConstructor
 @Builder
 class Person {
+    private String name;
+    private int height;
+
+    public boolean over(int pivot) {
+        return height >= pivot;
+    }
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+class Cat {
     private String name;
     private int height;
 
